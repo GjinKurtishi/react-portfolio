87a330531d83a7deb423cb238490c78f7f972e87
gjinkurtishi <gjin.kurtishi@gmail.com>
2022-10-13 10:42:43 +0200

    Testimonials added and SwiperJs added too

--- src/componentes/testimonials/Testimonials.js
+++ src/componentes/testimonials/Testimonials.js
@@ -1,8 +1,46 @@
 import React from "react";
 import "./testimonials.css";
+import {data} from  './testimonialsData'
+
+// import Swiper core and required modules
+import { Pagination, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 const Testimonials = () => {
-    return <section id='Testimonials'>Testimonials</section>;
+    return (
+        <section id='Testimonials'>
+            <h5>Review from clients</h5>
+            <h2>Testimonials</h2>
+
+            <Swiper className='container testimonials__container'
+                // install Swiper modules
+                    modules={[ Pagination ]}
+                    spaceBetween={40}
+                    slidesPerView={1}
+                    pagination={{ clickable: true }}>
+                {
+                    data.map((data) => {
+                        return (
+                            <SwiperSlide className='testimonial'>
+                                <div className='client__avatar'>
+                                    <img src={data.avatar} alt='Avatar'/>
+                                </div>
+                                <h5 className='client__name'>{data.name}</h5>
+                                <small className='client__review'>{data.review}</small>
+                            </SwiperSlide>
+                        );
+                        }
+                    )
+                }
+            </Swiper>
+        </section>
+    );
 };
 
 export default Testimonials;
---
+++ src/componentes/testimonials/testimonialsData.js
@@ -0,0 +1,40 @@
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+const data = [
+    {
+        avatar: AVTR1,
+        name: 'Name1',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ' +
+            'Corrupti saepefacilis ratione deserunt at consectetur delectus officia ' +
+            'porro et eum sed,asperiores provident quibusdam tenetur illo. ' +
+            'Vitae temporibus essedignissimos.'
+    },
+    {
+        avatar: AVTR2,
+        name: 'Name2',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ' +
+            'Corrupti saepefacilis ratione deserunt at consectetur delectus officia ' +
+            'porro et eum sed,asperiores provident quibusdam tenetur illo. ' +
+            'Vitae temporibus essedignissimos.'
+    },
+    {
+        avatar: AVTR3,
+        name: 'Name3',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ' +
+            'Corrupti saepefacilis ratione deserunt at consectetur delectus officia ' +
+            'porro et eum sed,asperiores provident quibusdam tenetur illo. ' +
+            'Vitae temporibus essedignissimos.'
+    },
+    {
+        avatar: AVTR4,
+        name: 'Name4',
+        review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ' +
+            'Corrupti saepefacilis ratione deserunt at consectetur delectus officia ' +
+            'porro et eum sed,asperiores provident quibusdam tenetur illo. ' +
+            'Vitae temporibus essedignissimos.'
+    }
+]
+export {data}
