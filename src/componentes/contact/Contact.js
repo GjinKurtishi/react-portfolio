344cac58bbfd17d5c78ea65dc560a368022707ba
gjinkurtishi <gjin.kurtishi@gmail.com>
2022-10-14 12:48:08 +0200

    Contact-Styling Added

--- src/componentes/contact/Contact.js
+++ src/componentes/contact/Contact.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./contact.css";
+import {MdOutlineMail} from 'react-icons/md';
+import {RiMessengerLine} from 'react-icons/ri';
+import {BsWhatsapp} from 'react-icons/bs';
+import {data} from './contactData';
 
 const contact = () => {
-    return <section id="contact">contact</section>;
+    return <section id="contact">
+        <h5>Get In Touch</h5>
+        <h2>Contact Me</h2>
+        <div className='container contact__container'>
+            <div className='contact__options'>
+                {
+                    data.map((props) => {
+                        return <article className='contact__option'>
+                                <MdOutlineMail className='contact__option-icon'/>
+                                <h4>{props.h4}</h4>
+                                <h5>{props.h5}</h5>
+                                <a href={props.link} target='_blank'>Send a message</a>
+                            </article>;
+                        }
+                    )
+                }
+            </div>
+            {/*END OF CONTACT OPTIONS*/}
+            <form action=''>
+                <input type='text' name='name' placeholder='Full Name' required />
+                <input type='email' name='email' placeholder='Email' required />
+                <textarea name='message' rows='7' placeholder='Message' required></textarea>
+                <button type='submit' className='btn btn-primary'>Send Message</button>
+            </form>
+        </div>
+    </section>;
 };
 
 export default contact;
---
+++ src/componentes/contact/contactData.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+        h4:'Email',
+        h5:'gjin.kurtishi@gmail.com',
+        link:'gjin.kurtishi@gmail.com'
+    },
+    {
+        h4:'Messenger',
+        h5:'gjinkurtishi',
+        link:'https://m.me/gjinkurtishi/'
+    },
+    {
+        h4:'WhatsApp',
+        h5:'Number',
+        link:'https://api.whatsapp.com/send?phone+38970987544'
+    }
+]
+
+export {data}
