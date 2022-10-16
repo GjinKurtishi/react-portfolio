142fb5443ced9097e4bb9aee418fdb22dae7234b
gjinkurtishi <gjin.kurtishi@gmail.com>
2022-10-16 23:32:46 +0200

    Setted up the email popup

--- src/componentes/contact/Contact.js
+++ src/componentes/contact/Contact.js
@@ -1,18 +1,24 @@
-import React, { useRef } from 'react';
+import React, {useRef, useState} from 'react';
 import "./contact.css";
-import {MdOutlineMail} from 'react-icons/md';
-import {RiMessengerLine} from 'react-icons/ri';
-import {BsWhatsapp} from 'react-icons/bs';
+import {FaCheckCircle} from 'react-icons/fa';
 import {data} from './contactData';
 import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const [popout, setPopout] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
+
         const form = useRef();
 
         const sendEmail = (e) => {
             e.preventDefault();
 
             emailjs.sendForm('service_e4lzn3f', 'template_tldl2ih', form.current, 'Vjg8SBT8qBeCBgmdC')
+                .then((result) => {
+                    setSubmitted(true);
+                }, (error) => {
+                    setSubmitted(false);
+                });
 
             e.target.reset();
         };
@@ -39,7 +45,13 @@ const Contact = () => {
                 <input type='text' name='name' placeholder='Full Name' required />
                 <input type='email' name='email' placeholder='Email' required />
                 <textarea name='message' rows='7' placeholder='Message' required></textarea>
-                <button type='submit' className='btn btn-primary'>Send Message</button>
+                <button type='submit' className='btn btn-primary' onClick={() => setPopout(true)}>Send Message</button>
+                <div id='popup' className={popout & submitted ? 'popup open-popup' : 'popup'}>
+                    <FaCheckCircle className='popup-icon'/>
+                    <h2>Thank You!</h2>
+                    <p>Your message has been successfully submitted. Thanks!</p>
+                    <button className='btn btn-primary' onClick={() => {setPopout(false); setSubmitted(false);}}>OK</button>
+                </div>
             </form>
         </div>
     </section>;
